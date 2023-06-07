f5cac5d16995150385f187390363ff1543346a0d
Motevalli Soumehsaraei <mot032@csiro.au>
2023-06-07 15:42:21 +0800

    simple apps

---
+++ test-api-1/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import JSONResponse, HTMLResponse
+
+app = FastAPI()
+
+@app.get("api/1/healthcheck")
+async def healthcheck():
+    return {"message": "Hello from API 1"}
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",  # Path to the OpenAPI schema
+        title="FastAPI - Swagger UI"
+    )
+
+@app.get("/openapi.json", include_in_schema=False)
+async def get_open_api_endpoint():
+    return JSONResponse(
+        get_openapi(
+            title="FastAPI - API 1",
+            version="1.0.0",
+            description="This is a simple 'Test' API. No 1."
+        )
+    )
